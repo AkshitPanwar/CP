cfa404641caefc0ca24ac5b11d6825b304cf348d
AkshitPanwar <62929980+AkshitPanwar@users.noreply.github.com>
2022-06-23 16:25:11 +0530

    Update Z algo.java

--- Z algo.java
+++ Z algo.java
@@ -1,8 +1,1 @@
-vector<int> z_function_trivial(string s) {
-    int n = (int) s.length();
-    vector<int> z(n);
-    for (int i = 1; i < n; ++i)
-        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
-            ++z[i];
-    return z;
-}
+
