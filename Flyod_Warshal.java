ebb58c6344d1d59ac805e49cfad2dab520029776
AkshitPanwar <62929980+AkshitPanwar@users.noreply.github.com>
2022-07-07 00:21:08 +0530

    Update Flyod_Warshal.java

--- Flyod_Warshal.java
+++ Flyod_Warshal.java
@@ -1,4 +1,30 @@
+IT HELPS TO CALCULATE SHORTEST PATH BETWEEN ANY PAIR OF NODES
 
+ALGO :-
+
+TAKE A dis[][] MATRIX --- dis[i][j] stores min distance bw i and j
+
+INITIALLY if there is a direct edge from i to j then, initialize dis[i][j] by that distance
+
+NOW FROM K = 1, K = N
+CONSIDER ANY PAIR (i, j) NOW THE MINIMUM WEIGHT FROM i to j WILL BE PASSING THROUGH SOME VERTICES.
+THIS IS THE WHOLE MAIN COCNEPT
+
+K = 1 MEANS FOR EVERY PAIR (i,j) WE CAN PASS THROUGH 1.
+K = 2 MEANS FOR EVERY PAIR (i,j) WE CAN PASS THROUGH 1,2
+K = 3 MEANS FOR EVERY PAIR (i,j) WE CAN PASS THROUGH 1,2,3
+.....
+K = N MEANS FOR EVERY PAIR (i,j) WE CAN PASS THROUGH 1,2,3,...N.
+
+let k = p;
+then dp[i][j] = min distance i to j when we can pass through 1 to p - 1.
+Now, just see if we can pass through p then can we minimise the distance.
+dp[i][j] = Math.min(dp[i][j], dp[i][p] + dp[p][j]).
+
+T.C. -> O(N * N * N)
+-> It can handle negative weights but cannot handle negative cycles.
+
+CODE :-
     		int N = 510;
 		int dp[][] = new int[N][N];
 		int INF = (int)1e9 + 10;
@@ -38,19 +64,4 @@
 				}
 			}
 		}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
