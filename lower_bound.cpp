46b5a11b079543f5edbf9bac417afe4bf951751a
AkshitPanwar <62929980+AkshitPanwar@users.noreply.github.com>
2022-10-02 09:18:01 +0530

    Update lower_bound.cpp

--- lower_bound.cpp
+++ lower_bound.cpp
@@ -1,25 +1,25 @@
 int lower_bound(vector<int>& a, int target)
 {
-        int l = 0, r = a.size() - 1;
-        if(a[0] >= target)
-        {
-            return 0;
-        }
-        if(a[r] < target)
-        {
-            return a.size();
-        }
-        while(r > l + 1)
-        {
-            int mid = (l + r) / 2;
-            if(a[mid] >= target)
-            {
-                r = mid;
-            }
-            else
-            {
-                l = mid;
-            }
-        }
-        return r;
+    int l = 0, r = a.size() - 1;
+    if(a[0] >= target)
+    {
+        return 0;
     }
+    if(a[r] < target)
+    {
+        return a.size();
+    }
+    while(r > l + 1)
+    {
+        int mid = (l + r) / 2;
+        if(a[mid] >= target)
+        {
+           r = mid;
+        }
+        else
+        {
+           l = mid;
+        }
+     }
+    return r;
+}
